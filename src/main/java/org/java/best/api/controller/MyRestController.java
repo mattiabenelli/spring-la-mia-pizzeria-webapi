c602e1ad5fb550c4bf8e4bd04ad41043119525ec
mattiabenelli <benellimattia0@gmail.com>
2023-06-07 17:39:25 +0200

    pizza show

--- src/main/java/org/java/best/api/controller/MyRestController.java
+++ src/main/java/org/java/best/api/controller/MyRestController.java
@@ -1,6 +1,7 @@
 package org.java.best.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.java.best.pojo.Pizza;
 import org.java.best.service.ServicePizza;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +42,13 @@ public class MyRestController {
 		List<Pizza> pizzas = pizzaService.findByNome(nome);
 		
 		return new ResponseEntity<>(pizzas, HttpStatus.OK);
+	}
+	
+	@GetMapping("/pizza/{id}")
+	public ResponseEntity<Pizza> getPizzaById(@PathVariable int id){
+		
+		Pizza optPizza = pizzaService.findById(id).get();
+		
+		return new ResponseEntity<>(optPizza, HttpStatus.OK);
 	}
 }
