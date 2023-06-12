b642e80fca8af79836b77f09201ad4912ef2f39e
mattiabenelli <benellimattia0@gmail.com>
2023-06-12 09:35:54 +0200

    collegamento api front back

---
+++ crud-front-end/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HomeView from "./views/HomeView.vue";
+</script>
+
+<template>
+  <main>
+    <HomeView></HomeView>
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ crud-front-end/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script>
+import axios, { Axios } from "axios";
+
+const BASE_API_URL = "http://localhost:8080/api/v1/";
+
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    getPizza() {
+      axios.get(BASE_API_URL + "/pizza").then();
+    },
+  },
+};
+</script>
+
+<template>
+  <main>helloworld</main>
+</template>
